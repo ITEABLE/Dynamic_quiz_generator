354fc99715f3448fbad46a6d3dc1675ee909df96
ITEABLE <63301675+ITEABLE@users.noreply.github.com>
2023-12-06 02:49:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,107 +1,3 @@
-# import streamlit as st
-# import langchain  # Mock library for generating options
-# from langchain.llms import OpenAI
-# from dotenv import load_dotenv
-# import os
-
-# from langchain.chat_models import ChatOpenAI
-# from langchain.prompts.chat import (
-#     ChatPromptTemplate,
-#     HumanMessagePromptTemplate,
-#     SystemMessagePromptTemplate,
-# )
-# from langchain.schema import HumanMessage, SystemMessage
-# # Set your OpenAI API key
-# load_dotenv()
-# OpenAI.api_key = os.environ.get("OPENAI_API_KEY")
-
-#     # Mock function for OpenAI API to generate questions
-# def generate_question(prompt):
-#     llm = OpenAI()
-    
-#     response = llm.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=[{"role": "user", "content": "Generate a multiple choice question  on {prompt} and generate 4 answers and show the correct answer"}],
-#         temperature=0.5,
-#         max_tokens=64,
-#         top_p=1
-#     )
-#     return response
-
-# chat = ChatOpenAI(temperature=0.1)
-# messages = [
-   
-#     HumanMessage(
-#         content="say hello"
-#     ),
-# ]
-# chat.invoke(messages)   
-
-
-# # Streamlit UI
-
-# def display_quiz(questions):
-#     # Clear existing widgets with the same key
-#     st.session_state.pop("user_answers", None)
-    
-#     # Display questions and get user answers
-#     user_answers = []
-#     for i, question in enumerate(questions, start=1):
-#         st.subheader(f"Question {i}: {question}")
-#         #selected_option = st.radio("Select your answer:", options, key=f"radio_{i}")
-#         user_answers.append({"question": question})
-
-#     if st.button("Submit Answers"):
-#         st.session_state.user_answers = user_answers
-#         return user_answers
-#     else:
-#         return []
-
-# def calculate_score(questions, user_answers):
-#     # Calculate the score based on user answers
-#     score = 0
-#     for i, question in enumerate(questions):
-#         if i < len(user_answers) and user_answers[i]['user_answer'] == question['options'][0]:
-#             score += 1
-#     return score
-
-# def main():
-#     st.title("Dynamic Quiz Generator")
-
-#     # User input
-#     topic = st.text_input("Enter the quiz topic:")
-#     quiz_questions =[]
-#     num_questions = st.number_input("How many questions do you want?", min_value=1, step=1)
-#  # Loop to generate each question
-#     if st.button("Generate Quiz"):
-#         for i in range(1, num_questions + 1):
-#             # Construct a prompt for generating quiz questions
-           
-            
-#             # Call OpenAI API to generate quiz question
-#             question = generate_question(topic)
-            
-#             # Append the generated question to the list
-#             quiz_questions.append(question)
-
-        
-#             # Call a function to fetch dynamic questions using Langchain and OpenAI
-    
-#             # Display questions and get user answers
-#             user_answers = display_quiz(quiz_questions)
-
-#         # Calculate and display the score
-
-#     # if st.button("Submit Answers", on_click="WidgetCallBack"):
-#     #     score = calculate_score(quiz_questions, user_answers)
-#     #     st.success(f"Your score: {score}/{len(quiz_questions)}")
-# if __name__ == "__main__":
-#     main()
-
-
-####################################################################################
-
-
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
@@ -109,15 +5,13 @@ import os
 from dotenv import load_dotenv  
 
 def generate_questions(topic, num_questions):
-    load_dotenv()
-    chat= ChatOpenAI(temperature=0.1)
+    load_dotenv() # load variables present in .env file
+    chat = ChatOpenAI(temperature=0.1) # connect to the openai api key with the default variable name to be "OPENAI_API_KEY" , and temprature means creativity in making the questions
     prompt = f"""generate {num_questions} multiple choice questions on {topic} with 4 options to each question but only one answer from them and give me the  answer,
     And make the Question no matter how long on one line, and the options after them in a line each option"""
     messages = [SystemMessage(content=prompt)]
     questions = chat.invoke(messages)
-    # print("++++++++++++++++++++++++++")
-    # print(questions.content)
-    # print("+++++++++++++++++++++++")
+ 
     generated_information = questions.content
     y = generated_information.split("\n")
     while('' in y):
@@ -126,6 +20,7 @@ def generate_questions(topic, num_questions):
     generated_question = {}
     options = {}
     answers = {}
+    
     j=1
 
     for i in range (0,len(y),6):
@@ -134,17 +29,16 @@ def generate_questions(topic, num_questions):
 
     j=1
    
-   
     for i in range (1,len(y),6):
         options[j] = [y[i],y[i+1],y[i+2],y[i+3]]
         j+=1
+        
     j=1
 
     for i in range (5,len(y),6):
         answers[j] = y[i]
         j+=1
 
-  
     return {"generated_questions" : generated_question ,"options": options,"answers":answers }
 
 def main():
@@ -154,15 +48,11 @@ def main():
     topic = st.text_input("Enter the quiz topic:")
     num_questions = st.number_input("How many questions do you want?", min_value=1, step=1)
     if st.button("Generate Quiz"):
-        # Call a function to fetch dynamic questions using OpenAI
         questions = generate_questions(topic, num_questions)
-        # Display questions and answers
         display_quiz(questions,num_questions)
-        
 
 
 def display_quiz(questions, num_questions):
-    # Display questions and get user answers
     with st.form("quiz_form"):
         user_answers = []
         for i in range(1, num_questions + 1):
@@ -171,8 +61,7 @@ def display_quiz(questions, num_questions):
             selected_option = st.radio(f"Select your answer for Question {i}:", options)
             user_answers.append(selected_option)
 
-        # Use st.form_submit_button and check if the button text is present in the form submission
-        if st.form_submit_button("Submit") in st.session_state:
+        if st.form_submit_button("Submit"):
             print("Inside submit block")
             answers = questions['answers']
             for i in range(len(answers)):
@@ -182,9 +71,7 @@ def display_quiz(questions, num_questions):
             score = calculate_score(answers, user_answers)
             st.write(f"Your score: {score}/{num_questions}")
 
-
 def calculate_score(answers, user_answers):
-    # Calculate the score based on user answers
     score = 0
     for i in range(len(answers)):
         if answers[i+1] ==user_answers[i]:
@@ -193,12 +80,8 @@ def calculate_score(answers, user_answers):
 
 if __name__ == "__main__":
     main()
-
 ####################################################################################
  
-
-
-
 #  {
 #      'generated_questions': 
 #      {
@@ -225,8 +108,3 @@ if __name__ == "__main__":
 #         5: 'Answer: a) 3.14'
 #         }
 #     }
-
-
-
-
-
